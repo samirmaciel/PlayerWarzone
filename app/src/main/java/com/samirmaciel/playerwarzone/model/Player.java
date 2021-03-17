1277d8159a38c5fcb2af341e871dd81208c63677
Samir Maciel <samir.maciel17@gmail.com>
2021-03-17 20:24:04 -0300

    criada a classe com os dados do player

---
+++ app/src/main/java/com/samirmaciel/playerwarzone/model/Player.java
@@ -0,0 +1,143 @@
+package com.samirmaciel.playerwarzone.model;
+
+import androidx.annotation.NonNull;
+
+public class Player {
+    private String nickname;
+    private String platform;
+    private String level;
+    private String prestige;
+    private String winsBR;
+    private String kills;
+    private String deaths;
+    private String downs;
+    private String score;
+    private String contracts;
+    private String kd;
+    private String gametime;
+    private String matchs;
+    private String headshots;
+
+    public Player(String nickname, String platform) {
+        this.nickname = nickname;
+        this.platform = platform;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getPrestige() {
+        return prestige;
+    }
+
+    public void setPrestige(String prestige) {
+        this.prestige = prestige;
+    }
+
+    public String getWinsBR() {
+        return winsBR;
+    }
+
+    public void setWinsBR(String winsBR) {
+        this.winsBR = winsBR;
+    }
+
+    public String getKills() {
+        return kills;
+    }
+
+    public void setKills(String kills) {
+        this.kills = kills;
+    }
+
+    public String getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(String deaths) {
+        this.deaths = deaths;
+    }
+
+    public String getDowns() {
+        return downs;
+    }
+
+    public void setDowns(String downs) {
+        this.downs = downs;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(String contracts) {
+        this.contracts = contracts;
+    }
+
+    public String getKd() {
+        return kd;
+    }
+
+    public void setKd(String kd) {
+        this.kd = kd;
+    }
+
+    public String getGametime() {
+        return gametime;
+    }
+
+    public void setGametime(String gametime) {
+        this.gametime = gametime;
+    }
+
+    public String getMatchs() {
+        return matchs;
+    }
+
+    public void setMatchs(String matchs) {
+        this.matchs = matchs;
+    }
+
+    public String getHeadshots() {
+        return headshots;
+    }
+
+    public void setHeadshots(String headshots) {
+        this.headshots = headshots;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return getNickname();
+    }
+}
